64afb3cd7efb8abe144d57b893f623497f39757e
Kreabox <id.kreabox@gmail.com>
2024-06-11 02:11:16 +0700

    Adding Info Aplikasi

--- services/save_sekolah.php
+++ services/save_sekolah.php
@@ -1,1 +1,5 @@
-<?php include_once $_SERVER['DOCUMENT_ROOT'].'/services/connection.php'; ?>
+<?php 
+include_once $_SERVER['DOCUMENT_ROOT'].'/services/connection.php'; 
+
+
+?>
--- services/save_user.php
+++ services/save_user.php
@@ -1,8 +1,17 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/services/connection.php'; ?>
 <?php 
-
-if($_POST['submit']){
-
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['role'])){
+    $nama = $_POST['nama'];
+    $username = $_POST['username'];
+    $role = $_POST['role'];
+    $password = sha1($_POST['password']);
+    $sql = "INSERT INTO users (nama, username, role, password) VALUES('$nama','$username','$role', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        header('Location: /app/daftar_user.php'); 
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();    
 }else{
     echo "Terjadi Kesalahan Saat Menambah Data";
 }
